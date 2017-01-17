0c43dea4af525a4a9d60db73bc8f24b62cfa637e
Noctisae <fanch.toquer@laposte.net>
2017-01-17 19:34:55 +0100

    Ajout de quelques fonctions DNS pour windows Server

---
+++ DNS/Functions.ps1
@@ -0,0 +1,55 @@
+Function Add-DNSEntryA{
+    param([String]$ZoneName="microsoft.com",
+    [String]$EntryName="$env:computername",
+    [String]$IPAddress="test")
+
+    try{
+        Import-Module DNSServer;
+        if($IPAddress -eq "test"){
+            $Networks = (Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $env:computername -EA Stop | ? {$_.IPEnabled}).IPAddress
+            $Networks | ForEach-Object {
+                if($_ -match "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+                {
+                    $IPAddress = $_;
+                    return;
+                }
+            }
+        }
+        if($IPAddress){
+            Write-Host "IP Address supplied or found, adding DNS Record"
+            $dns = Get-DnsServerResourceRecord -ZoneName $ZoneName -Name $EntryName
+            if($dns){
+                Remove-DnsServerResourceRecord -ZoneName $ZoneName -RRType "A" -Name $EntryName
+            }
+            Add-DnsServerResourceRecordA -Name $EntryName -ZoneName $ZoneName -IPv4Address $IPAddress
+        }
+        else{
+            Write-Host "No IPV4 Address found on this computer, exiting...."
+        }
+    }
+    catch{
+        $ErrorMessage = $_.Exception.Message
+        Write-Host $ErrorMessage
+    }
+
+}
+
+
+Function Add-DNSEntryCName{
+    param([String]$ZoneName="microsoft.com",
+    [String]$EntryName="$env:computername",
+    [String]$HostNameAlias ="alias.$env:computername")
+    try{
+        Import-Module DNSServer;
+        $dns = Get-DnsServerResourceRecord -ZoneName $ZoneName -Name $EntryName
+        if($dns){
+            Remove-DnsServerResourceRecord -ZoneName $ZoneName -RRType "CName" -Name $EntryName
+        }
+        Add-DnsServerResourceRecordA -Name $EntryName -ZoneName $ZoneName -HostNameAlias $HostNameAlias
+    }
+    catch{
+        $ErrorMessage = $_.Exception.Message
+        Write-Host $ErrorMessage
+    }
+
+}
